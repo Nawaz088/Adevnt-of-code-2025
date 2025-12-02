81979cf18c51ab43386c77ed60928fce0954def9
Nawaz088 <abdulmuqeetnawaz@gmail.com>
2025-12-02 10:32:54 +0530

    Day1 part2

--- Day1/sol.js
+++ Day1/sol.js
@@ -1,23 +1,40 @@
 const fs = require('fs');
 
 // Read input from file
-const input = fs.readFileSync('Day1/input.txt', 'utf-8').split('\n');
+const input = fs.readFileSync('Day1/SampleInput.txt', 'utf-8').split('\n');
 
 let start = 50;
 let count = 0;
+let a = [...Array(100).keys()];
 
+// Part 1
+// let GoLeftBy = (num) => {
+//     let res = (start - num + 100)%100;
+//     if( res === 0 ) count++;
+//     start = res;
+//     return res;
+// }
+
+// let GoRightBy = (num) => {
+//     let res = (start + num)%100;
+//     if( res === 0 ) count++;
+//     start = res;
+//     return res;
+// }
+
+// Part 2
 let GoLeftBy = (num) => {
-    let res = (start - num + 100)%100;
-    if( res === 0 ) count++;
-    start = res;
-    return res;
+    for(let i = 0; i < num; i++){
+        start = (start-1+100)%100;
+        if(start == 0){ count++; }
+    }
 }
 
 let GoRightBy = (num) => {
-    let res = (start + num)%100;
-    if( res === 0 ) count++;
-    start = res;
-    return res;
+    for(let i= 0; i < num; i++){
+        start = (start+1)%100;
+        if(start == 0) count++;
+    }
 }
 
 for (let line of input) {
