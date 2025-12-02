f256c88633e743ef963ea63d7d724c3d5298c094
Nawaz088 <abdulmuqeetnawaz@gmail.com>
2025-12-02 13:09:06 +0530

    Day2 part1 and part2

---
+++ Day2/sol.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+// Read input from file
+const input = fs.readFileSync('Day2/input.txt', 'utf-8').split(',');
+
+let range = [];
+const isPalendrom = (from, to) => {
+    for(let i = Number(from); i <= Number(to); i++){
+        // part 1 
+        // const reg = /b(\d+)\1{1}/b/;
+        // part 2
+        const reg = /\b(\d+)\1+\b/;
+        if(reg.test(String(i))){
+            range.push(i);
+        }
+    }
+}
+
+
+for(let range of input){
+    let [from, to] = range.split('-');
+    console.log("Processing range:", from, to);
+
+    isPalendrom(from, to);
+}
+
+let total = range.reduce((acc, val) => acc + val, 0);
+
+console.log("Total palendroms found:", total);
+
+
+// resources used:
+// https://stackoverflow.com/questions/6507982/regex-to-find-repeating-numbers
